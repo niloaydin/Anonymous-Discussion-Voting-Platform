3ff48f35a53c8e99e3c99b9a6f60433fd33279df
naydin <naydin@natera.com>
2024-12-24 23:28:25 +0300

    add user discussion page

---
+++ frontend/src/pages/UserDiscussionPage.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Spin, notification } from "antd";
+import axios from "axios";
+import { setDiscussion } from "../features/discussionSlice";
+import DiscussionCard from "../components/DiscussionCard";
+import BASE_URL from "../config/baseUrl";
+const UserDiscussionPage = () => {
+    const { discussionLink, userLink } = useParams();
+    const dispatch = useDispatch();
+    const discussion = useSelector((state) => state.discussion.currentDiscussion);
+    const [newCommentAdded, setNewCommentAdded] = useState(false); 
+
+    useEffect(() => {
+        const fetchDiscussion = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/discussion/${discussionLink}/${userLink}`);
+                dispatch(setDiscussion(response.data.message));
+            } catch (error) {
+                notification.error({
+                    message: "Error",
+                    description: error.response?.data?.error || "Failed to load discussion.",
+                });
+            }
+        };
+
+        if (!discussion || discussion.link !== discussionLink) {
+            fetchDiscussion();
+        }
+    }, [dispatch, discussionLink, newCommentAdded]);
+
+    if (!discussion) {
+        return (
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+                <Spin size="large" />
+            </div>
+        );
+    }
+
+    return (
+        <div style={{ maxWidth: 1000, margin: "auto", marginTop: 50 }}>
+            <DiscussionCard discussion={discussion} />
+            <button
+                className="ant-btn"
+                style={{ marginTop: 20, display: "block", margin: "0 auto" }}
+                onClick={() => window.location.href = "/"}
+            >
+                Create Your Own Discussion
+            </button>
+        </div>
+    );
+};
+
+export default UserDiscussionPage;
